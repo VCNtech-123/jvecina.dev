e09f6765cc4a21ce4834aad475a67eccdd094f95
VCNtech-123 <johnvecina640@gmail.com>
2026-08-27 04:01:43 +0800

    feat: add error middleware for centralized error handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,15 @@
 import express from 'express'
 import { securityMiddleware } from './middleware/security.middleware';
+import cookieParser from 'cookie-parser'
+import { errorMiddleware } from './middleware/error.middleware';
 
 
 const app = express()
 
 app.use(express.json())
 app.use(securityMiddleware)
+app.use(cookieParser())
+
+app.use(errorMiddleware)
 
 export default app;
---
+++ server/src/middleware/error.middleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import { ApiError } from "../utils/ApiError";
+
+export const errorMiddleware = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.error("ACTUAL RUNTIME ERROR CAUGHT:", err);
+
+  if (err instanceof ApiError) {
+    return res.status(err.statusCode).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+    errorDetails: err.message || err, 
+    stack: err.stack
+  });
+};
