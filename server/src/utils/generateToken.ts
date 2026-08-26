6569053d205004e92a9b465a374ba677770b5a84
VCNtech-123 <johnvecina640@gmail.com>
2026-08-27 04:00:40 +0800

    feat: refactor generateToken to use dynamic expiration time from environment variables

--- server/src/utils/generateToken.ts
+++ server/src/utils/generateToken.ts
@@ -1,10 +1,12 @@
-import jwt from 'jsonwebtoken'
+import jwt, { SignOptions } from 'jsonwebtoken'
 import { env } from '../config/env'
+
+const expiresIn = (env.expiresIn) as SignOptions['expiresIn'];
 
 export const generateToken = (userId: string) => {
     return jwt.sign(
         { id: userId },
         env.jwt as string,
-        { expiresIn: '1d' }
+        { expiresIn }
     )
 }
