72e620000bc95a669ecc3d6c86985e9b82f7d233
VCNtech-123 <johnvecina640@gmail.com>
2026-08-27 04:38:11 +0800

    feat: add authentication routes for login and registration with validation

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express'
-import { securityMiddleware } from './middleware/security.middleware';
 import cookieParser from 'cookie-parser'
+import { securityMiddleware } from './middleware/security.middleware';
 import { errorMiddleware } from './middleware/error.middleware';
+import authRoutes from './modules/auth/auth.routes'
 
 
 const app = express()
@@ -10,6 +11,7 @@ app.use(express.json())
 app.use(securityMiddleware)
 app.use(cookieParser())
 
+app.use("/api/auth", authRoutes)
 app.use(errorMiddleware)
 
 export default app;
