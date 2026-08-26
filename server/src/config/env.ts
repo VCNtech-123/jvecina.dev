f6d3d6161a924a259761ff82bdfa594c97eb86d2
VCNtech-123 <johnvecina640@gmail.com>
2026-08-27 03:49:47 +0800

    feat: create environment configuration file for managing environment variables

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -5,6 +5,7 @@ dotenv.config()
 
 export const env = {
     mongoUri: process.env.MONGO_URI as string,
-    port: process.env.PORT || 5000
+    port: process.env.PORT || 5000,
+    jwt: process.env.JWT_KEY as string
 }
 
