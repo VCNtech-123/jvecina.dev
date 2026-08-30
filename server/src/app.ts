c8743edf9bc28c1afdfaddd8bd795c38cd6e4c56
VCNtech-123 <johnvecina640@gmail.com>
2026-08-31 03:10:32 +0800

    feat: update app and env configuration for improved API routing and environment variables

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,22 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import { env } from './config/env';
 import { securityMiddleware } from './middleware/security.middleware';
 import { errorMiddleware } from './middleware/error.middleware';
 import authRoutes from './modules/auth/auth.routes'
 import projectRoutes from './modules/projects/project.routes'
 import messageRoutes from './modules/messages/message.routes'
+import adminRoutes from './modules/admin/admin.routes'
 
 
 const app = express()
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173", 
+    env.clientUrl
+  ],
   credentials: true
 }));
 app.use(express.json())
@@ -21,6 +26,7 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/projects", projectRoutes)
 app.use("/api/messages", messageRoutes)
+app.use("/api/admin", adminRoutes)
 app.use(errorMiddleware)
 
 export default app;
